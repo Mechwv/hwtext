333e7338aa56f80313f03b91310b88dceb7a4b9c
Mechwv <55464927+Mechwv@users.noreply.github.com>
2019-12-11 00:37:56 +0300

    hw text 2)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,10 @@ public class Main {
 
     public static void main(String[] args){
         File f = new File("D://Android projects/A.txt");
-        File o = new File("D://Android projects/B.txt");
-        PrintWriter out = null;
         String pool;
         int cnt=0;
         try {
             f.createNewFile();
-            o.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -21,7 +18,6 @@ public class Main {
         String text = scan.nextLine();
         try {
             PrintWriter writer = new PrintWriter(new FileWriter(f));
-            out = new PrintWriter(new FileWriter(o));
             writer.printf(text);
             writer.close();
         } catch (IOException e) {
@@ -31,11 +27,11 @@ public class Main {
             Scanner s = new Scanner(f);
             while (s.hasNext()){
                 pool = s.next();
-                if (pool.charAt(0) == 'a'){
-                    out.append(pool+" ");
+                if (pool.charAt(pool.length()-1) == 'b'){
+                    cnt++;
                 }
             }
-            out.close();
+            System.out.println(cnt);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
